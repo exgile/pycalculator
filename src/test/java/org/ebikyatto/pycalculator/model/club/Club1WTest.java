70d801d6fe5a52820b4cfb6b2c022278beaec337
ebikyatto <ebikyatto@gmail.com>
2015-08-18 00:12:21 +0900

    fix assert equals result

--- src/test/java/org/ebikyatto/pycalculator/model/club/Club1WTest.java
+++ src/test/java/org/ebikyatto/pycalculator/model/club/Club1WTest.java
@@ -35,8 +35,36 @@ public class Club1WTest extends TestCase {
 		Result result = club1W.tomahawk(environment);
 		this.getLogger().debug(result.toString());
 
-		Assert.assertEquals(23.487, result.getPbScale(), .1);
-		Assert.assertEquals(4.6975, result.getMoca(), .1);
+		Assert.assertEquals(23.412, result.getPbScale(), .1);
+		Assert.assertEquals(4.682, result.getMoca(), .1);
 		Assert.assertEquals(92.972, result.getForce(), .1);
+	}
+	
+	@Test
+	public void testTomahawkEnhance() {
+		Environment environment = new Environment();
+
+		double yard = 260;
+		double elevation = -10;
+		double[] speedOfWinds = new double[] {9};
+		double[] angleOfWinds = new double[] {45};
+		double[] ratioOfWinds = new double[] {1};
+		double breakOfDip = -2;
+		double yardOfMaxDip = 260;
+		
+		environment.setYard(yard);
+		environment.setElevation(elevation);
+		environment.setSpeedOfWinds(speedOfWinds);
+		environment.setAngleOfWinds(angleOfWinds);
+		environment.setRatioOfWinds(ratioOfWinds);
+		environment.setBreakOfDip(breakOfDip);
+		environment.setYardOfMaxDip(yardOfMaxDip);
+		
+		Result result = club1W.tomahawkEnhance(environment);
+		this.getLogger().debug(result.toString());
+
+		Assert.assertEquals(23.412, result.getPbScale(), .1);
+		Assert.assertEquals(4.682, result.getMoca(), .1);
+		Assert.assertEquals(90.388, result.getForce(), .1);
 	}
 }
