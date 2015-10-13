65176c779f912022cd49ecb829c93a6b9c6e466d
ebikyatto <ebikyatto@gmail.com>
2015-10-13 11:22:11 +0800

    Removes modifier

--- src/main/java/ebikyatto/pycalculator/common/interfaces/SpecialShot.java
+++ src/main/java/ebikyatto/pycalculator/common/interfaces/SpecialShot.java
@@ -5,22 +5,22 @@ import ebikyatto.pycalculator.model.vo.Result;
 
 public interface SpecialShot {
 	
-	public static final String TOMAHAWK = "TOMAHAWK";
-	public static final String BACKSPIN = "BACKSPIN";
-	public static final String DUNK = "DUNK";
-	public static final String COBRA = "COBRA";
-	public static final String SPIKE = "SPIKE";
+	String TOMAHAWK = "TOMAHAWK";
+	String BACKSPIN = "BACKSPIN";
+	String DUNK = "DUNK";
+	String COBRA = "COBRA";
+	String SPIKE = "SPIKE";
 	
-	public Result tomahawk(Environment environment);
-	public Result tomahawkEnhance(Environment environment);
-	public Result dunk(Environment environment);
-	public Result dunkPowerful(Environment environment);
-	public Result dunkEnhance(Environment environment);
-	public Result backspin(Environment environment);
-	public Result backspinPowerful(Environment environment);
-	public Result backspinEnhance(Environment environment);
-	public Result cobra(Environment environment);
-	public Result cobraEnhance(Environment environment);
-	public Result spike(Environment environment);
-	public Result spikeEnhance(Environment environment);
+	Result tomahawk(Environment environment);
+	Result tomahawkEnhance(Environment environment);
+	Result dunk(Environment environment);
+	Result dunkPowerful(Environment environment);
+	Result dunkEnhance(Environment environment);
+	Result backspin(Environment environment);
+	Result backspinPowerful(Environment environment);
+	Result backspinEnhance(Environment environment);
+	Result cobra(Environment environment);
+	Result cobraEnhance(Environment environment);
+	Result spike(Environment environment);
+	Result spikeEnhance(Environment environment);
 }
